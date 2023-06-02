b48ac2aad03b617a6de0b5e41136006deeae8b43
Oscar Idano <vergil.ferrer.jp@gmail.com>
2023-06-02 23:18:57 +0900

    removed unused code

--- orders/urls.py
+++ orders/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.order_list, name="orders"),
-    path("new_old/", views.create_order, name="new-order-old"),
-    path("new/", views.create_order_v2, name="new-order"),
+    path("new/", views.create_order, name="new-order"),
     path("products/", views.get_products, name="products-all"),
     path("product_prices/", views.current_prices, name="product-prices"),
     path("print_order/", views.print_order, name="print-order"),
